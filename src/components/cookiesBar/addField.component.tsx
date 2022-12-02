3d9551ac917bb9e7862c3d846202f29bd1805abd
Jeremy Lardenois <jeremy.lardenois@etu.umontpellier.fr>
2022-12-02 06:02:25 +0100

    feat (cookies): it's very funny

--- src/components/cookiesBar/addField.component.tsx
+++ src/components/cookiesBar/addField.component.tsx
@@ -1,12 +1,59 @@
 import { useState } from "react"
+import { useForm } from "react-hook-form"
+// import { styled } from "styled-components"
 
-export const AddFields = () => {
+
+export const FormFields = ({ setStep }) => {
+    const [error, setError] = useState("")
+    const onclick = () => {
+        if (document.getElementById("form3")) {
+            if ((document.getElementById("form0")?.value == "V") &&
+                (document.getElementById("form1")?.value == "III") &&
+                (document.getElementById("form2")?.value == "II") &&
+                (document.getElementById("form3")?.value == "I")) {
+                setStep("final")
+            }
+
+            else setError("Tu n'as pas retiré tous les cookies")
+        } else setError("Il te manque des éléments...")
+        console.log(error)
+    }
+    const onSubmit = (data: any) => {
+        alert(JSON.stringify(data))
+    }
+
+    const [hover, setHover] = useState(false)
+    const [left, setLeft] = useState(false)
     const [nbFields, setNbFields] = useState(1)
-    const incrementNbFields = () => setNbFields(nbFields+1)
-    return <>
-        {/* {for (let i = 0; i < nbFields; i++){
-            <span onClick={incrementNbFields}> + </span>
-            <input type="text"/>
-        }} */}
-        </>
+    const incrementNbFields = () => setNbFields(nbFields + 1)
+    const fields = []
+    for (let i = 0; i < nbFields; i++) {
+        if (i < 4) fields.push('_')
+    }
+    return <div>
+        {
+            fields.map((object, i) =>
+                <div style={{
+                    marginTop: "3px"
+                }} key={i}>
+                    <span onClick={incrementNbFields}> + </span>
+                    <input type="text" id={"form" + i} />
+                </div>
+            )}
+        <p style={{ color: "red" }}>Vous ne devriez pas ajouter plus d'éléments</p>
+        <div style={{ display: "flex", justifyContent: left?"start":"end" }}>
+            <button
+                onMouseEnter={() => {setHover(true); setLeft(!left)}}
+                onMouseLeave={() => setHover(false)}
+                style={{ backgroundColor: hover ? "yellow" : "blue", }}
+                onClick={onclick}
+            >
+                Valider
+            </button>
+        </div>
+        <div style={{ display: "flex", justifyContent: "end" }}>
+            <p style={{ color: "blue" }}>{error}</p>
+        </div>
+        
+    </div>
 }
